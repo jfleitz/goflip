3323647c4b73d75ce2a1ed86d002b60b01d20f62
jfleitz <jeremy.fleitz@gmail.com>
2017-05-17 22:11:21 -0400

    diagnostics

--- web/score.js
+++ web/score.js
@@ -3,14 +3,16 @@ app.controller("scoreCtrl", function ScoreControl($scope,$http){
 
 $scope.connectWS = function(){
     var s = gotalk.connection().on('open',function(){
+		$scope.$apply(function () {
         $scope.connectionStatus="Connected";
         $scope.messages = new Array;
-
+		});
     });
 };
 
 gotalk.handleNotification('stat', function(scores){
     var js = JSON.parse(scores);
+	$scope.$apply(function () {
     $scope.Player1Score = js.Player1Score ;
 	$scope.Player2Score = js.Player2Score ;
 	$scope.Player3Score = js.Player3Score ;
@@ -23,18 +25,21 @@ gotalk.handleNotification('stat', function(scores){
 	$scope.Display3     = js.Display3     ;
 	$scope.Display4     = js.Display4     ;
 	$scope.Credits      = js.Credits      ;
-
+	});
 });
 
 
 gotalk.handleNotification('msg', function(logEvent){
     var js = JSON.parse(logEvent);
-
+$scope.$apply(function () {
     $scope.messages.push(js);
+});
 });
 
 $scope.clearEvents = function(){
+	
  $scope.messages = new Array;
+	
 }
 
 
