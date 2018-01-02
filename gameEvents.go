d5b6691c1abf717859f591d021b6a70f338587b4
jfleitz <jeremy.fleitz@gmail.com>
2018-01-01 22:29:29 -0500

    change to better handle player change

--- gameEvents.go
+++ gameEvents.go
@@ -2,11 +2,31 @@ package goflip
 
 import log "github.com/Sirupsen/logrus"
 
+/*
+gameEvents has handlers for the game in play.
+
+Player Control events:
+GameStart = called when a credit is added to the machine (someone presses the credit button)
+AddPlayer = adds to the number of players in the game
+PlayerUp = called every time that a new player is up
+PlayerEnd = called every time a player loses the ball in play
+PlayerFinish = called at the very end of the game for that player
+GameOver = called at the very end of the game
+
+BallInPlay events:
+BallDrained = called when a ball is now found in the outhole
+BallInPlay = called when a ball is launched
+*/
+
 //GameStart is called when a game is started (when the first player gets a credit)
 func (g *GoFlip) GameStart() {
-	log.Infoln("GameStart() called")
+	log.Infoln("gameEvents:GameStart()")
+	//Reset game stats
 	g.BallInPlay = 0 //nextup will queue this up
+	g.NumOfPlayers = 0
 	g.CurrentPlayer = 0
+
+	g.GameRunning = true
 
 	for _, f := range g.Observers {
 		f.GameStart()
@@ -16,13 +36,15 @@ func (g *GoFlip) GameStart() {
 
 //GameOver should be called when it is game over.
 func (g *GoFlip) GameOver() {
-	log.Infoln("GameOver() called")
+	log.Infoln("gameEvents:GameOver()")
 
 	g.BallInPlay = 0
 
 	for _, f := range g.Observers {
 		f.GameOver()
 	}
+
+	g.GameRunning = false
 }
 
 func (g *GoFlip) BallDrained() {
@@ -33,36 +55,47 @@ func (g *GoFlip) BallDrained() {
 	}
 }
 
+func (g *GoFlip) PlayerFinish() {
+	log.Infoln("PlayerFinish() called")
+
+	for _, f := range g.Observers {
+		f.PlayerFinish(g.CurrentPlayer)
+	}
+}
+
 func (g *GoFlip) PlayerEnd() {
 	log.Infoln("PlayerEnd() called")
 
 	for _, f := range g.Observers {
 		f.PlayerEnd(g.CurrentPlayer)
 	}
+
+	g.PlayerUp() //call for the next ball or player if there is one
 }
 
-func (g *GoFlip) NextUp() {
-	log.Infoln("NextUp() called")
+func (g *GoFlip) PlayerUp() {
+	log.Infoln("PlayerUp() called")
+	g.BallScore = 0 //reset before any points are added
 
 	defer func() {
-		if g.IsGameInPlay() {
+		if g.GameRunning {
 			for _, f := range g.Observers {
 				f.PlayerUp(g.CurrentPlayer)
 			}
-			log.Infoln("GoFlip: NextUp. IsGameInPlay. BallInPlay, CurrentPlayer:", g.BallInPlay, g.CurrentPlayer)
+			log.Infoln("GoFlip: PlayerUp. IsGameInPlay. BallInPlay, CurrentPlayer:", g.BallInPlay, g.CurrentPlayer)
 		} else {
-			log.Infoln("GoFlip: NextUp. IsGameInPlay = false")
+			log.Infoln("GoFlip: PlayerUp. IsGameInPlay = false")
 		}
 
 	}()
 
 	if g.BallInPlay == 0 {
-		g.BallInPlay = 1
+		//first time we are playing
+		//g.BallInPlay = 1
 		g.CurrentPlayer = 1
-		return
 	}
 
-	if g.CurrentPlayer < g.MaxPlayers {
+	if g.CurrentPlayer < g.NumOfPlayers {
 		g.CurrentPlayer++ //we are staying on the same ball
 	} else {
 		//next ball
@@ -71,9 +104,31 @@ func (g *GoFlip) NextUp() {
 			g.CurrentPlayer = 1
 		} else {
 			//game over
+			g.SetBallInPlayDisp(0)
 			g.GameOver()
 			return
 		}
 	}
 
+	g.SetBallInPlayDisp(int8(g.BallInPlay))
+
+	if g.BallInPlay == 1 {
+		for _, f := range g.Observers {
+			f.PlayerStart(g.CurrentPlayer)
+		}
+		return
+	}
+}
+
+func (g *GoFlip) AddPlayer() {
+	log.Infoln("AddPlayer() called")
+	//sanity check, only can add a player if on ball 1
+	if g.BallInPlay > 1 {
+		return
+	}
+
+	if g.NumOfPlayers < g.MaxPlayers {
+		g.NumOfPlayers++
+		g.ShowDisplay(g.NumOfPlayers, true)
+	}
 }
